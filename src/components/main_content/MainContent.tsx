a7c821b2518825904f983b89fc7d618b0e23ff52
cathino <onlydev@sina.com>
2025-03-22 22:10:41 +0800

    表名列表正常了

---
+++ src/components/main_content/MainContent.tsx
@@ -0,0 +1,11 @@
+import { MainContentData } from "@/types/types";
+
+export function MainContent(props: MainContentData) {
+  return (
+    <div>
+      <div>标签页(表结构 / DDL /数据)</div>
+      <div>次级标签页(表结构 /数据 的显示)</div>
+      <div>表格的数据</div>
+    </div>
+  );
+}
