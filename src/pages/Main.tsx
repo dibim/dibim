5d7c0925bcd3d6f8e4a8580dcd253b2b39064539
cathino <onlydev@sina.com>
2025-03-24 07:34:02 +0800

    优化侧边栏

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,41 +1,60 @@
 import { useCallback, useEffect, useRef, useState } from "react";
+import { PanelLeftIcon } from "lucide-react";
+import { Separator } from "@radix-ui/react-separator";
 import { MainContent } from "@/components/main_content/MainContent";
 import { TableList } from "@/components/sub_idebar/TableList";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import { Button } from "@/components/ui/button";
+import { useSidebar } from "@/components/ui/sidebar";
 import { DB_TYPE_POSTGRES_SQL, MAIN_CONTEN_TYPE_TABLE_EDITOR } from "@/constants";
 import { connect } from "@/databases/PostgreSQL/utils";
+import { cn } from "@/lib/utils";
 import { useCoreStore } from "@/store";
 
 export function Main() {
-  const [sidebarWidth, setSidebarWidth] = useState(240);
+  const {
+    currentDbType,
+    setCurrentDbType,
+    setMainContenType,
+    currentDbNme,
+    currentTableName,
+    sidebarOpen,
+    setSidebarOpen,
+  } = useCoreStore();
+
+  const { toggleSidebar, setOpenMobile, setOpen } = useSidebar();
+
+  // ========== 控制次级侧边栏 ==========
+  const [subSidebarWidth, setSubSidebarWidth] = useState(240);
   const [isDragging, setIsDragging] = useState(false);
   const mouseInitialPosX = useRef(0); // 起始坐标
-
-  const { currentDbType, setCurrentDbType, setMainContenType } = useCoreStore();
-
   const handleMouseDown = useCallback(() => {
     setIsDragging(true);
   }, []);
-
   const handleMouseMove = useCallback(
     (e: MouseEvent) => {
       if (!isDragging) return;
-
       if (mouseInitialPosX.current === 0) {
         mouseInitialPosX.current = e.clientX; // 记录起始坐标
       }
 
       // 计算新宽度
-      const newWidth = sidebarWidth + e.clientX - mouseInitialPosX.current;
-      setSidebarWidth(newWidth);
+      const newWidth = subSidebarWidth + e.clientX - mouseInitialPosX.current;
+      setSubSidebarWidth(newWidth);
     },
     [isDragging],
   );
-
   const handleMouseUp = useCallback(() => {
     setIsDragging(false);
     mouseInitialPosX.current = 0; // 重置起始坐标
   }, []);
-
   useEffect(() => {
     document.addEventListener("mousemove", handleMouseMove);
     document.addEventListener("mouseup", handleMouseUp);
@@ -45,6 +64,7 @@ export function Main() {
       document.removeEventListener("mouseup", handleMouseUp);
     };
   }, [handleMouseMove, handleMouseUp]);
+  // ========== 控制次级侧边栏 结束 ==========
 
   // TODO: 库,差是否有连接配置, 有的话显示 MainContent ,没有的话显示 Index
 
@@ -65,33 +85,74 @@ export function Main() {
 
   useEffect(() => {
     testDb();
+
+    setOpenMobile(sidebarOpen);
+    setOpen(sidebarOpen);
   }, []);
 
   return (
-    <div className="flex">
-      {/* 次级侧边栏 */}
-      <div className="flex border-r relative" style={{ width: sidebarWidth }}>
-        {/* 次级侧边栏内容 */}
-        {/* 这里的 height 是屏幕高度减去 header 的高度 */}
-        <div
-          className="flex-1 overflow-y-scroll py-2 ps-2 pe-4"
-          style={{ height: "calc(100vh - var(--spacing) * 12)" }}
+    <>
+      <header className="flex h-12 shrink-0 items-center gap-2 border-b px-4">
+        {!sidebarOpen && <span className="text-xl font-semibold cursor-pointer">DIBIM</span>}
+
+        {/* 复制 sidebar-trigger 过来, 这里添加了函数, 记录 sidebar 的状态*/}
+        <Button
+          data-sidebar="trigger"
+          data-slot="sidebar-trigger"
+          variant="ghost"
+          size="icon"
+          className={cn("size-7")}
+          onClick={(event) => {
+            setOpenMobile(!sidebarOpen);
+            setOpen(!sidebarOpen);
+            toggleSidebar();
+
+            setSidebarOpen(!sidebarOpen);
+          }}
         >
-          {/* TODO: 链接对应的数据库并查询表格 */}
-          {currentDbType === DB_TYPE_POSTGRES_SQL && <TableList />}
+          <PanelLeftIcon />
+          <span className="sr-only">Toggle Sidebar</span>
+        </Button>
+
+        <Separator orientation="vertical" className="mr-2 h-4" />
+        <Breadcrumb>
+          <BreadcrumbList>
+            <BreadcrumbItem className="hidden md:block">
+              <BreadcrumbLink href="#">{currentDbNme}</BreadcrumbLink>
+            </BreadcrumbItem>
+            <BreadcrumbSeparator className="hidden md:block" />
+            <BreadcrumbItem>
+              <BreadcrumbPage>{currentTableName}</BreadcrumbPage>
+            </BreadcrumbItem>
+          </BreadcrumbList>
+        </Breadcrumb>
+      </header>
+
+      <div className="flex">
+        {/* 次级侧边栏 */}
+        <div className="flex border-r relative" style={{ width: subSidebarWidth }}>
+          {/* 次级侧边栏内容 */}
+          {/* 这里的 height 是屏幕高度减去 header 的高度 */}
+          <div
+            className="flex-1 overflow-y-scroll py-2 ps-2 pe-4"
+            style={{ height: "calc(100vh - var(--spacing) * 12)" }}
+          >
+            {/* TODO: 链接对应的数据库并查询表格 */}
+            {currentDbType === DB_TYPE_POSTGRES_SQL && <TableList />}
+          </div>
+
+          {/* 拖拽手柄 */}
+          <div
+            className="absolute -right-1 top-0 w-2 h-full cursor-col-resize hover:bg-primary/50"
+            onMouseDown={handleMouseDown}
+          />
         </div>
 
-        {/* 拖拽手柄 */}
-        <div
-          className="absolute -right-1 top-0 w-2 h-full cursor-col-resize hover:bg-primary/50"
-          onMouseDown={handleMouseDown}
-        />
+        {/* 主内容区域 */}
+        <div className="flex-1 p-4">
+          <MainContent />
+        </div>
       </div>
-
-      {/* 主内容区域 */}
-      <div className="flex-1 p-4">
-        <MainContent />
-      </div>
-    </div>
+    </>
   );
 }
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="vite/client" />
+import "react";
+
+// 给 style 属性添加 允许 css 变量名
+declare module "react" {
+  interface CSSProperties {
+    [key: `--${string}`]: string | number | undefined;
+  }
+}
