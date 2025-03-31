17255d541e9b0080de921e0bac43d45da19b8587
cathino <364936+cathino@user.noreply.gitee.com>
2025-03-31 08:30:38 +0800

    优化sql高亮

--- src/components/SqlCodeViewer.tsx
+++ src/components/SqlCodeViewer.tsx
@@ -1,12 +1,24 @@
 import { useEffect, useRef } from "react";
 import hljs from "highlight.js/lib/core";
+import pgsql from "highlight.js/lib/languages/pgsql";
 import sql from "highlight.js/lib/languages/sql";
 import "highlight.js/styles/tokyo-night-dark.css";
+import { DB_TYPE_POSTGRESQL } from "@/constants";
+import { useCoreStore } from "@/store";
 
 hljs.registerLanguage("sql", sql);
+hljs.registerLanguage("postgresql", pgsql);
 
 export function SqlCodeViewer(props: { ddl: string }) {
+  const { currentDbType } = useCoreStore();
   const codeRef = useRef<HTMLElement>(null);
+
+  // TODO: add support for SQLite, MySQ, Oracle
+  function getLang() {
+    if (currentDbType === DB_TYPE_POSTGRESQL) return "postgresql";
+
+    return "sql";
+  }
 
   useEffect(() => {
     if (codeRef.current) {
@@ -17,7 +29,7 @@ export function SqlCodeViewer(props: { ddl: string }) {
 
   return (
     <pre>
-      <code ref={codeRef} className={`language-sql`}>
+      <code ref={codeRef} className={`language-${getLang()}`}>
         {props.ddl}
       </code>
     </pre>
