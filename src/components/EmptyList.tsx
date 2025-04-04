54cc7f58f8c07ad59a1e0bbfc03305d0970474c7
cathino <onlydev@sina.com>
2025-04-04 14:48:49 +0800

    feat(SQL viewer): optimize

--- src/components/EmptyList.tsx
+++ src/components/EmptyList.tsx
@@ -3,8 +3,10 @@ import { Skeleton } from "./ui/skeleton";
 export function EmptyList() {
   return (
     <div className="py-4">
-      <Skeleton className="h-4 w-[150px]" />
-      <Skeleton className="h-4 w-[100px]" />
+      <Skeleton className="h-4 w-[180px] m-2" />
+      <Skeleton className="h-4 w-[150px] m-2" />
+      <Skeleton className="h-4 w-[100px] m-2" />
+      <Skeleton className="h-4 w-[160px] m-2" />
     </div>
   );
 }
--- src/components/SqlCodeViewer.tsx
+++ src/components/SqlCodeViewer.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import hljs from "highlight.js/lib/core";
 import pgsql from "highlight.js/lib/languages/pgsql";
 import sql from "highlight.js/lib/languages/sql";
@@ -11,9 +11,10 @@ import { formatSql } from "@/utils/format_sql";
 hljs.registerLanguage("sql", sql);
 hljs.registerLanguage("postgresql", pgsql);
 
-export function SqlCodeViewer(props: { ddl: string }) {
+export function SqlCodeViewer({ ddl }: { ddl: string }) {
   const snap = useSnapshot(appState);
   const codeRef = useRef<HTMLElement>(null);
+  const [sql, setSql] = useState<string>(""); // 美化后的 sql
 
   // TODO: add support for SQLite, MySQ, Oracle
   function getLang() {
@@ -27,12 +28,21 @@ export function SqlCodeViewer(props: { ddl: string }) {
       delete codeRef.current.dataset.highlighted;
       hljs.highlightElement(codeRef.current);
     }
-  }, [props]);
+  }, [sql]);
+
+  useEffect(() => {
+    const res = formatSql(snap.currentDbType, ddl);
+    if (res.errorMessage === "") {
+      setSql(res.result);
+    } else {
+      setSql(res.errorMessage);
+    }
+  }, [ddl]);
 
   return (
     <pre>
       <code ref={codeRef} className={`language-${getLang()}`}>
-        {formatSql(snap.currentDbType, props.ddl).result}
+        {sql}
       </code>
     </pre>
   );
