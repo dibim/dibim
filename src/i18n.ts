83d7c6d047a217ac778e97c30f9d72d9e63077f4
cathino <onlydev@sina.com>
2025-04-06 21:39:17 +0800

    i18n: add browser language auto-detection

--- src/i18n.ts
+++ src/i18n.ts
@@ -3,8 +3,18 @@ import i18n from "i18next";
 import enTranslation from "./locales/en/translation.json";
 import zhTranslation from "./locales/zh/translation.json";
 
+const getBrowserLanguage = () => {
+  const userLanguage = navigator.language;
+  const lang = userLanguage.split("-")[0]; // 简化为语言代码前两位（如 'zh-CN' → 'zh'）
+  if (["en", "zh"].includes(lang)) {
+    return lang;
+  }
+
+  return "en";
+};
+
 i18n.use(initReactI18next).init({
-  lng: "zh",
+  lng: getBrowserLanguage(),
   fallbackLng: "en",
   interpolation: {
     escapeValue: false,
